2955524875ccfb213d70f252f0aa8250033aa3bc
Anmol Ranjan <anmolranjan1@gmail.com>
2024-11-16 21:54:47 +0530

    Added Login and Sign Up

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import logo from "../logo.svg";
+
+const Header = () => {
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img
+        className="w-44"
+        src={logo}
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
